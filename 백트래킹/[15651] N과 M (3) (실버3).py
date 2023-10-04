22be93f8586db000c2fa520199beaf8ccacbc528
taeho0888 <taeho0888@naver.com>
2023-10-04 17:00:34 +0900

    231004

---
+++ 백트래킹/[15651] N과 M (3) (실버3).py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/15651
+
+N, M = map(int, input().split())
+
+stack = []
+def dfs():
+    if len(stack) == M:
+        print(" ".join(map(str, stack)))
+        return
+    
+    for i in range(1, N+1):
+        stack.append(i)
+        dfs()
+        stack.pop()
+
+dfs()
